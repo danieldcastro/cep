386ce2f4f2ede5e465c08dbad806327b056b0552
Daniel Marciano <daniel.castro09@hotmail.com>
2022-08-11 16:13:06 -0300

    🍱Asset: Added icons and images

---
+++ lib/core/util/helpers/icon_paths.dart
@@ -0,0 +1,12 @@
+class IconPaths {
+  static const String bookmark = 'assets/icons/bookmark.png';
+  static const String copy = 'assets/icons/copy.png';
+  static const String heart = 'assets/icons/heart.png';
+  static const String home = 'assets/icons/home.png';
+  static const String menuStack = 'assets/icons/menu_stack.png';
+  static const String people = 'assets/icons/people.png';
+  static const String search = 'assets/icons/search.png';
+  static const String signpost = 'assets/icons/signpost.png';
+  static const String star = 'assets/icons/star.png';
+  static const String trash = 'assets/icons/trash.png';
+}
---
+++ lib/core/util/helpers/image_paths.dart
@@ -0,0 +1,4 @@
+class ImagePaths {
+  static const String manAndMap = 'assets/images/man_and_map.png';
+  static const manWithBackpack = 'assets/images/man_with_backpack.png';
+}
