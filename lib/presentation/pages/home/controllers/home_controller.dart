792a0c3d350111af2140f6c42a8c042037a15485
Daniel Marciano <daniel.castro09@hotmail.com>
2022-08-11 19:51:58 -0300

    🍱 Assets: Added svg images and icons

--- lib/presentation/pages/home/controllers/home_controller.dart
+++ lib/presentation/pages/home/controllers/home_controller.dart
@@ -1,7 +1,13 @@
 import 'package:get/get.dart';
 
+import '../../../routes/routes.dart';
+
 class HomeController extends GetxController {
   RxInt searchCounter = 0.obs;
 
   RxInt savedCounter = 0.obs;
+
+  goToHistoryPage() {
+    Get.offNamed(Routes.HISTORY);
+  }
 }
