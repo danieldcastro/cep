b9ad989cfe9a905e7dd3e9ec2c34b81d411ef2af
Daniel Marciano <daniel.castro09@hotmail.com>
2022-08-12 19:01:16 -0300

    🔨 Datasource: Created datasource and implementation

---
+++ lib/infrastructure/provider/datasource.dart
@@ -0,0 +1,9 @@
+import 'package:cep/domain/contracts/gateways/http_service.dart';
+import 'package:either_dart/either.dart';
+
+import '../errors/failure_request.dart';
+
+abstract class Datasource {
+  Future<Either<FailureRequest, HttpResponse>> getCepInfoByCepNumber(
+      String cepNumber);
+}
