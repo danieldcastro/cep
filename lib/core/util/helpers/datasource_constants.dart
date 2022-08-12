910cbe00a0c357c7eedb1c7deac422887d81eb3b
Daniel Marciano <daniel.castro09@hotmail.com>
2022-08-12 18:28:58 -0300

    🔨 Helper: Created a file to organize all datasource constants

---
+++ lib/core/util/helpers/datasource_constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+class DatasourceConstants {
+  static const CEP_API_URL = 'https://viacep.com.br/ws/';
+
+  static const CEP_API_RESPONSE_TYPE = '/json';
+}
