3650aa72fd22dff2df5372c933bfcd2e5270d7cb
Daniel Marciano <daniel.castro09@hotmail.com>
2022-08-12 19:00:50 -0300

    🔨 repository: Created get cep repository and implementation

---
+++ lib/infrastructure/contracts/repositories/get_cep_repository_impl.dart
@@ -0,0 +1,28 @@
+import 'package:cep/domain/entities/cep_entity.dart';
+import 'package:either_dart/either.dart';
+
+import '../../../domain/contracts/repositories/get_cep_repository.dart';
+import '../../errors/failure_request.dart';
+import '../../models/remote_cep_model.dart';
+import '../../provider/datasource.dart';
+
+class GetCepRepositoryImpl implements GetCepRepository {
+  final Datasource _datasource;
+
+  GetCepRepositoryImpl({required Datasource datasource})
+      : _datasource = datasource;
+
+  @override
+  Future<Either<FailureRequest, CepEntity>> call(String cepNumber) async {
+    final result = await _datasource.getCepInfoByCepNumber(cepNumber);
+    if (result.isLeft) {
+      return Left(result.left);
+    } else {
+      final cep = RemoteCepModel.fromJson(result.right.data);
+
+      final cepModelToEntity = CepEntity.fromModel(cep);
+
+      return Right(cepModelToEntity);
+    }
+  }
+}
