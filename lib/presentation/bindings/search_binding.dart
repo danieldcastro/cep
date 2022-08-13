4644e8ba07389f3b91ce12aee2ed22162661018d
Daniel Marciano <daniel.castro09@hotmail.com>
2022-08-12 21:04:22 -0300

    🔨 Controller: Updated search controller and binding to implement usecase

--- lib/presentation/bindings/search_binding.dart
+++ lib/presentation/bindings/search_binding.dart
@@ -1,3 +1,5 @@
+import 'package:cep/domain/usecases/get_cep_info_by_cep_number/get_cep_info_by_cep_number_usecase_impl.dart';
+import 'package:cep/infrastructure/contracts/repositories/get_cep_repository_impl.dart';
 import 'package:get/get.dart';
 
 import '../pages/search/controllers/search_controller.dart';
@@ -5,6 +7,9 @@ import '../pages/search/controllers/search_controller.dart';
 class SearchBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<SearchController>(() => SearchController());
+    Get.lazyPut<SearchController>(() => SearchController(
+        logger: Get.find(),
+        cepInfoByCepNumberUsecase: GetCepInfoByCepNumberUsecaseImpl(
+            repository: GetCepRepositoryImpl(datasource: Get.find()))));
   }
 }
--- lib/presentation/pages/search/controllers/search_controller.dart
+++ lib/presentation/pages/search/controllers/search_controller.dart
@@ -1,26 +1,65 @@
+import 'package:cep/core/util/global_show_snackbar_function.dart';
+import 'package:cep/domain/entities/cep_entity.dart';
+import 'package:cep/domain/usecases/get_cep_info_by_cep_number/get_cep_info_by_cep_number_usecase.dart';
+import 'package:cep/presentation/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../../domain/contracts/gateways/my_logger.dart';
+
 class SearchController extends GetxController {
+  CepEntity _cepEntity = CepEntity.empty();
+  RxBool isLoading = false.obs;
+
   TextEditingController searchTextFieldController = TextEditingController();
 
   RxBool isFavoriteButtonPressed = false.obs;
+  final GetCepInfoByCepNumberUsecase _cepInfoByCepNumberUsecase;
+  final MyLogger _logger;
 
-  RxDouble favoriteButtonStarTopPosition = 0.0.obs;
+  RxString foundAddress = ''.obs;
+
+  SearchController(
+      {required GetCepInfoByCepNumberUsecase cepInfoByCepNumberUsecase,
+      required MyLogger logger})
+      : _cepInfoByCepNumberUsecase = cepInfoByCepNumberUsecase,
+        _logger = logger;
 
   void onTapFavoriteButton() {
     isFavoriteButtonPressed.value = !isFavoriteButtonPressed.value;
-    isFavoriteButtonPressed.value
-        ? changeFavoriteButtonStarPositionDown()
-        : changeFavoriteButtonStarPositionUp();
   }
 
-  void changeFavoriteButtonStarPositionUp() {
-    favoriteButtonStarTopPosition = (-30.0).obs;
+  Future<void> getCepByCepNumber(String cepNumber) async {
+    isLoading.value = true;
+    _logger.info('searching cep: $cepNumber');
+    String cepNumberFormatted = cepNumber.replaceAll('-', '');
+    final result = await _cepInfoByCepNumberUsecase.call(cepNumberFormatted);
+
+    if (result.isRight) {
+      _cepEntity = result.right;
+      formatFoundAddress();
+      isLoading.value = false;
+    } else {
+      isLoading.value = false;
+      _logger.error('Error when searching cep: ${result.left}');
+      GlobalShowSnackbarFunction().show(
+          'Não foi possível procurar o cep $cepNumber',
+          color: AppColors().normalRedColor);
+    }
   }
 
-  void changeFavoriteButtonStarPositionDown() {
-    favoriteButtonStarTopPosition = (30.0).obs;
+  void formatFoundAddress() {
+    _cepEntity.complement.isNotEmpty
+        ? foundAddress.value =
+            '${_cepEntity.street} - ${_cepEntity.complement} - ${_cepEntity.city} ${_cepEntity.uf} - CEP ${_cepEntity.cep}'
+        : foundAddress.value =
+            '${_cepEntity.street} - ${_cepEntity.city} ${_cepEntity.uf} - CEP ${_cepEntity.cep}';
+  }
+
+  void submitSearch() {
+    if (searchTextFieldController.text.length == 9) {
+      getCepByCepNumber(searchTextFieldController.text);
+    }
   }
 
   @override
