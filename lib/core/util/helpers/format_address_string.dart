4962c4179c4f0741bab0538e6b688c58611fd532
Daniel Marciano <daniel.castro09@hotmail.com>
2022-08-13 23:23:48 -0300

    🔨 Global: Created global function to format address string

---
+++ lib/core/util/helpers/format_address_string.dart
@@ -0,0 +1,9 @@
+import 'package:cep/domain/entities/cep_entity.dart';
+
+class FormatAddressString {
+  static String formatByCepEntity(CepEntity cepEntity) {
+    return cepEntity.complement.isNotEmpty
+        ? '${cepEntity.street} - ${cepEntity.complement} - ${cepEntity.city} ${cepEntity.uf} - CEP ${cepEntity.cep}'
+        : '${cepEntity.street} - ${cepEntity.city} ${cepEntity.uf} - CEP ${cepEntity.cep}';
+  }
+}
