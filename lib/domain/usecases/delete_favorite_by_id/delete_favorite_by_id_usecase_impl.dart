940fdcb5d8c6391bca3ef0cf2dfab599a81c2adc
Daniel Marciano <daniel.castro09@hotmail.com>
2022-08-13 23:25:51 -0300

    🎨 Usecase: Created delete favorite abstraction and implementation

---
+++ lib/domain/usecases/delete_favorite_by_id/delete_favorite_by_id_usecase_impl.dart
@@ -0,0 +1,25 @@
+import 'package:cep/domain/contracts/repositories/favorite_repository.dart';
+import 'package:either_dart/either.dart';
+
+import 'delete_favorite_by_id_usecase.dart';
+
+class DeleteFavoriteByIdUsecaseImpl implements DeleteFavoriteByIdUsecase {
+  final FavoriteRepository _repository;
+
+  DeleteFavoriteByIdUsecaseImpl(this._repository);
+
+  @override
+  Future<Either<Exception, void>> call(int id) async {
+    if (id < 1) {
+      return Left(Exception('Invalid ID'));
+    }
+
+    final result = await _repository.delete(id);
+
+    if (result.isLeft) {
+      return Left(result.left);
+    }
+
+    return const Right(null);
+  }
+}
